5700004f7dd4a58a342284d8548fcbdffc851e62
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-04-20 22:12:26 +0300

    Create a simple project with Identity and role based authorization

---
+++ AspNetWithIdentity/AspNetWithIdentity/Data/Migrations/20220420183919_AddAdminRoleAndUser.cs
@@ -0,0 +1,18 @@
+﻿using AspNetWithIdentity.Data.Extensions;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetWithIdentity.Data.Migrations
+{
+    public partial class AddAdminRoleAndUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddRole("admin");
+            migrationBuilder.AddUserWithRoles("admin@gmail.com", "Admin123!", new[] { "admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
